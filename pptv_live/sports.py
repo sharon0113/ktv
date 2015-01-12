a44e11fd9a85b0258f6307cc731e495c95a5e122
weifanding <weifanding@yiqiding.com>
2015-01-12 10:41:52 +0800

    modified:   pptv_live/sports.py

--- pptv_live/sports.py
+++ pptv_live/sports.py
@@ -4,6 +4,7 @@ from sportsModel import sportsModel
 from django.db import connection
 from PPTVSportsSpider import PPTVSpider
 
+ROOT = "/m3u8/"
 
 def get_list(request):
 	cursor = connection.cursor()
@@ -13,4 +14,25 @@ def get_list(request):
 def spider(request):
 	cursor = connection.cursor()
 	date = request.GET.get("date", datetime.now().strftime("%Y-%m-%d"))
-	PPTVSpider(cursor).runSpider(date)
+	result = PPTVSpider(cursor).runSpider(date)
+	return Jsonify(result)
+def read_m3u8(request):
+	cursor = connection.cursor()
+	vid = request.GET.get("vid", 0)
+	url = sportsModel(cursor).get_url(vid)
+	if url:
+		fp = open("/m3u8/"+url, "r")
+		content = fp.read()
+	else:
+		content = ""
+	return Jsonify(content)
+def read_ts(request):
+	cursor=  connection.cursor()
+	vid = request.GET.get("vid", 0)
+	date =  request.GET.get("date", datetime.now().strftime("%Y-%m-%d"))
+	start = request.GET.get("start", 0)
+	during = request.GET.get("during", 10)
+	currentFeature = "start="+str(start)+"&during="+str(during)
+	fp = open(ROOT+"ts/"+date+"-"+str(vid)+"-"+currentFeature+".ts", "r")
+	content = fp.read()
+	return Jsonify(content)
