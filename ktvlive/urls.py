431d328e7fe20f94f1c227797a7fdc5405c8b9e3
weifanding <weifanding@yiqiding.com>
2015-01-13 18:33:42 +0800

    modified:   ktvlive/urls.py

--- ktvlive/urls.py
+++ ktvlive/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from pptv_live.sports import get_list,spider, read_m3u8, read_ts
+from pptv_live.sports import get_list,spider, read_m3u8, read_ts, get_precast
 urlpatterns = patterns("",
     # Examples:
     # url(r'^$', 'ktvlive.views.home', name='home'),
@@ -11,5 +11,6 @@ urlpatterns = patterns("",
     url(r'^pptvlive/spider',spider),
     url(r'^pptvlive/readm3u8[0-9]*.m3u',read_m3u8),
     url(r'^pptvlive/readts[0-9]*.ts',read_ts),
+    url(r'^pptvlive/getprecast',get_precast),
     #url(r'^pptvlive/getlist',include(get_list)),
 )
