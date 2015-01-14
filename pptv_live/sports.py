846d9eabe3d926fac73691ba0a594f0b7d5d01d8
weifanding <weifanding@yiqiding.com>
2015-01-14 14:08:49 +0800

    modified:   pptv_live/sports.py

--- pptv_live/sports.py
+++ pptv_live/sports.py
@@ -6,9 +6,10 @@ from django.db import connection
 from PPTVSportsSpider import PPTVSpider
 
 ROOT = "/mnt/m3u8/"
+M3U8NEWPATH = "/mnt/m3u8live/m3u8New/"
+TSPATH = "/mnt/m3u8live/ts/"
 
 def get_list(request):
-	cursor = connection.cursor()
 	date = request.GET.get("date", datetime.now().strftime("%Y-%m-%d"))
 	result = sportsModel().one_day_list(date)
 	return Jsonify(result)
@@ -20,9 +21,19 @@ def spider(request):
 	return Jsonify(result)
 
 def read_m3u8(request):
-	cursor = connection.cursor()
 	vid = request.GET.get("vid", 0)
 	url = sportsModel().get_url(vid)
+	if url:
+		with open(url) as fp:
+			content = fp.read()
+	else:
+		content = ""
+	return HttpResponse(content, content_type="application/vnd.apple.mpegurl")
+
+def read_live_m3u8(request):
+	vid = request.GET.get("vid", 0)
+	date = datetime.now().strftime("%Y-%m-%d")
+	url = M3U8NEWPATH+date+"-"+str(vid)+".m3u"
 	if url:
 		with open(url) as fp:
 			content = fp.read()
@@ -42,15 +53,22 @@ def read_ts(request):
 		content = fp.read()
 		return HttpResponse(content, content_type="video/MP2T")
 
+def read_llve_ts(request):
+	vid = request.GET.get("vid", "0")
+	tsCode = request.GET.get("tsCode", "00000X")
+	date =  request.GET.get("date", datetime.now().strftime("%Y-%m-%d"))
+	path = TSPATH+date+"-"+str(vid)+"-"+tsCode+".ts"
+	with open(path,'rb') as fp:
+		content = fp.read()
+		return HttpResponse(content, content_type="video/MP2T")
+
 def get_precast(request):
 	cursor=  connection.cursor()
 	date =  request.GET.get("date", datetime.now().strftime("%Y-%m-%d"))
 	result = PPTVSpider(cursor).getPrecastList(date)
 	return Jsonify(result)
 
-def read_live_m3u8(request):
-	pass
-
-def read_llve_ts(request):
-	pass
+def get_current_live(request):
+	infoList = liveModel().getLiveList(date)
+	return Jsonify(result)
 	
