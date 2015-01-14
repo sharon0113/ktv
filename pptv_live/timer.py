68ca8becf663f85d26ccaf7475256137bd6da4b3
weifanding <weifanding@yiqiding.com>
2015-01-14 12:04:52 +0800

    modified:   pptv_live/timer.py

--- pptv_live/timer.py
+++ pptv_live/timer.py
@@ -4,6 +4,7 @@ from PPTVSportsSpider import PPTVSportsSpider
 from Downloader import M3u8LiveDownloader
 import urllib2
 from time import sleep
+from daemonize import Daemonize
 
 PORT = "http://127.0.0.1:8000/"
 ROOT = "/mnt/m3u8/"
@@ -13,33 +14,43 @@ currentDate = "2015-01-13"
 InfoList = {}
 cursor = connection.cursor()
 
-while(True):
-	starttime = datetime.now()
-	lastDate = currentDate
-	currentDate = datetime.now().strftime("%Y-%m-%d")
-	currentTime = datetime.now().strftime("%H:%M")
-	#part only newday execute
-	if currentDate != lastDate:
-		print "new day "+ currentDate + "started"
-	#part every loop to examine
-	liveList = PPTVSportsSpider(cursor).getLiveList(currentDate)
-	currentInfolist = {}
-	for liveUrl in liveList:
-		if liveUrl in InfoList.keys():
-			currentInfolist[liveUrl] = InfoList[liveUrl]
+def runTimer():
+	while(True):
+		print "run it in "+datetime.now().strftime("%T")
+		starttime = datetime.now()
+		lastDate = currentDate
+		currentDate = datetime.now().strftime("%Y-%m-%d")
+		currentTime = datetime.now().strftime("%H:%M")
+		#part only newday execute
+		if currentDate != lastDate:
+			print "new day "+ currentDate + "started"
+		#part every loop to examine
+		liveList = PPTVSportsSpider(cursor).getLiveList(currentDate)
+		currentInfolist = {}
+		for liveUrl in liveList:
+			if liveUrl in InfoList.keys():
+				currentInfolist[liveUrl] = InfoList[liveUrl]
+			else:
+				currentInfolist[liveUrl] = set([])
+				InfoList[liveUrl] = currentInfolist[liveUrl]
+			currentSet = M3u8LiveDownloader(liveUrl, currentInfolist[liveUrl]).runDownloader()
+			InfoList[liveUrl] = currentSet
+		endtime = datetime.now()
+		delta = (endtime - starttime).total_seconds()
+		delta = int(delta)
+		if delta < 120:
+			remains = 120 - delta
+			sleep(remains)
 		else:
-			currentInfolist[liveUrl] = set([])
-			InfoList[liveUrl] = currentInfolist[liveUrl]
-		currentSet = M3u8LiveDownloader(liveUrl, currentInfolist[liveUrl]).runDownloader()
-		InfoList[liveUrl] = currentSet
-	endtime = datetime.now()
-	delta = (endtime - starttime).total_seconds()
-	delta = int(delta)
-	if delta < 120:
-		remains = 120 - delta
-		sleep(remains)
+			print "WARNING: while loop runs over 2 seconds."
 
-
+if __name__=='__main__':
+	pid="timer.pid"
+	
+	keep_fds = [fh.stream.fileno()]
+	# servermain()
+	daemon = Daemonize(app="jobs", pid=pid, action=runTimer,keep_fds=keep_fds)
+	daemon.start()
 	
 		
 
