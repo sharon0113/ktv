7a4931c3c869e5e3c990a3dc59ff4538069327ac
weifanding <weifanding@yiqiding.com>
2015-01-21 14:21:56 +0800

    modified:   sports.py

--- pptv_live/sports.py
+++ pptv_live/sports.py
@@ -37,12 +37,19 @@ def read_live_m3u8(request):
 	vid = request.GET.get("vid", 0)
 	date = datetime.now().strftime("%Y-%m-%d")
 	url = M3U8NEWPATH+date+"-"+str(vid)+".m3u"
-	if url:
-		with open(url) as fp:
-			content = fp.read()
-	else:
-		content = ""
-	return HttpResponse(content, content_type="application/vnd.apple.mpegurl")
+	while(True):
+		logger.debug("############DEBUG############")
+		logger.debug("request for m3u8"+path+"at:  "+datetime.now().strftime("%T"))
+		logger.debug("############DEBUG############")
+		try:
+			with open(url) as fp:
+				content = fp.read()
+			return HttpResponse(content, content_type="application/vnd.apple.mpegurl")
+		except Exception, e:
+			debug.logger(e)
+			debug.logger("301 can't get ts file")
+			sleep(1)
+			continue
 
 def read_ts(request):
 	cursor=  connection.cursor()
@@ -61,13 +68,15 @@ def read_live_ts(request):
 	tsCode = request.GET.get("tsCode", "00000X")
 	date =  request.GET.get("date", datetime.now().strftime("%Y-%m-%d"))
 	path = TSPATH+date+"-"+str(vid)+"-"+tsCode+".ts"
-	logger.debug(path)
 	logger.debug(datetime.now().strftime("%T"))
 	while(True):
+		logger.debug("############DEBUG############")
+		logger.debug("request for ts"+path+"at:  "+datetime.now().strftime("%T"))
+		logger.debug("############DEBUG############")
 		try:
 			with open(path,'rb') as fp:
 				content = fp.read()
-				return HttpResponse(content, content_type="video/MP2T")
+			return HttpResponse(content, content_type="video/MP2T")
 		except Exception, e:
 			debug.logger(e)
 			debug.logger("301 can't get ts file")
